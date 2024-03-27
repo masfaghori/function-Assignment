89246eaa671108a836e255d41a301d90aa47e430
masfaghori <161416552+masfaghori@users.noreply.github.com>
2024-03-27 21:36:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var fruitName = "Mango";
+console.log(fruitName);
+function logfruitName() {
+    var fruitName = "orange";
+    console.log(fruitName);
+}
+logfruitName();
+var carName = "Toyota";
+function logcarName() {
+    console.log(carName);
+}
+logcarName();
+// Practice : 1
+function logSomeNames(fruitName, carName, vegatable, makeup, electronicOItem) {
+    console.log(fruitName, carName, vegatable, makeup, electronicOItem);
+}
+logSomeNames("grapes", "toyota", "potato", "lipstick", "Tv");
+// Practice : 2
+function logDishesNames(desi, chinese, BBQ) {
+    console.log(desi, chinese, BBQ);
+}
+logDishesNames("biryani", "chowmien", "tikka");
+// Practise : 3
+function logCountryName(saudia, turkey, korea, pakistan, japan) {
+    console.log(saudia, turkey, korea, pakistan, japan);
+}
+logCountryName("riyadh", "istambul", "soul", "islamabad", "tokyo");
---
+++ main.ts
@@ -0,0 +1,39 @@
+let fruitName = "Mango";
+
+console.log(fruitName)
+
+function logfruitName() {
+    let fruitName = "orange";
+    console.log(fruitName)
+
+}
+
+logfruitName()
+
+let carName = "Toyota";
+
+function logcarName() {
+    console.log(carName)
+}
+logcarName()
+
+// Practice : 1
+
+function logSomeNames(fruitName, carName, vegatable, makeup, electronicOItem ){
+    console.log(fruitName, carName, vegatable, makeup, electronicOItem)
+}
+logSomeNames("grapes","toyota","potato","lipstick","Tv");
+
+// Practice : 2
+
+function logDishesNames(desi, chinese, BBQ, ){
+    console.log(desi, chinese, BBQ)
+}
+logDishesNames("biryani", "chowmien", "tikka");
+ 
+// Practise : 3
+
+function logCountryName(saudia, turkey, korea, pakistan, japan){
+    console.log(saudia, turkey, korea, pakistan, japan)
+}
+logCountryName("riyadh", "istambul", "soul", "islamabad", "tokyo");
